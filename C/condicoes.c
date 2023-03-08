33b516e75a51a941b0e88159a0c7e56863afab22
TheH4wk <davissupremp123@gmail.com>
2023-03-08 17:32:34 -0300

    c é legal

---
+++ C/condicoes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+  int a = 1;
+  if (a == 1) {
+    printf("A e igual a 1\n");
+  } else {
+    printf("A nao e igual a 1\n");
+  }
+  return 0;
+}
---
+++ C/ola_mundo.c
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+  std::cout << "Ola mundo" << "\n";
+  return 0;
+}
