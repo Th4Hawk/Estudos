5ab61f0dcf2a781cdfd93b1d01ff105a800a629a
Th4Hawk <whitereaper7777@protonmail.com>
2023-12-16 19:16:26 -0300

    Estudos C removidos.

---
+++ C++/condições.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int numero = 18;
+
+    if (numero > 18) {
+        std::cout << "Já pode beber" << "\n";
+    } else {
+        std::cout << "Não pode beber" << "\n";
+    }
+}
